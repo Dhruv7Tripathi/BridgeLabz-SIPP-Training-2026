134feba1991067993ccde8ccbf01b8d6e7c4f91e
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2026-06-16 09:10:27 +0530

    [Dhruv Tripathi] Solve Recursion Practice Assignment

---
+++ core-java-practice/Assignments/recursionPractice/printNoNtoOne.java
@@ -0,0 +1,14 @@
+public class printNoNtoOne {
+
+  static void printNto1(int n) {
+    if (n == 0) {
+      return;
+    }
+    System.out.print(n + " ");
+    printNto1(n - 1);
+  }
+
+  public static void main(String[] args) {
+    printNto1(5);
+  }
+}
